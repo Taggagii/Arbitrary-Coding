4212433409ef376a0869854bcf3336aa9524d86c
Joshua Dietrich <59072117+Taggagii@users.noreply.github.com>
2020-12-30 16:00:25 -0500

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -74,7 +74,7 @@ def downloadWord(word):
             sbValue += 1
             counter = 0
             for definition in definitionIndex:
-                definitions[counter].append(re.sub(" +", " ", defintion.text.replace("\n", "^")).replace("^ ^", "\n"))
+                definitions[counter].append(re.sub(" +", " ", definition.text).replace("\n \n", "\n"))
                 counter += 1
             
                 
