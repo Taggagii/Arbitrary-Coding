f3643018b93a0c019dffb584d131e51fe1898e94
Joshua Dietrich <59072117+Taggagii@users.noreply.github.com>
2021-09-29 15:11:54 -0400

    Create permute.py

---
+++ permute.py
@@ -0,0 +1,18 @@
+'''
+Recursively generates the permutations of a set length of lower case letters (eg. 2 => aa ab ba bb).
+Writes the output to a file. BE AWARE THIS CODE GENERATES REALLY LARGE FILES REALLY QUICKLY, SO DON'T PUT IN A NUMBER LARGER THAN 5 OR SO UNLESS YOU HAVE THE SPACE (which you don't) TO STORE IT.
+'''
+
+def permute(length: int, file, prev = None):
+    if prev == None:
+        prev = ""
+    if len(prev) == length:
+        return prev
+    for i in range(ord('a'), ord('z') + 1):
+        value = permute(length, file, prev + chr(i))
+        if type(value) == str:
+            file.write(value + ", ")
+
+permute(int(input("size: ")), open(input("filename: ") + ".txt", "a+"))
+print("done")
+input()
