853b2dcfebf8b8969fe573b3f1e50ac5e59e6522
Joshua Dietrich <59072117+Taggagii@users.noreply.github.com>
2021-09-29 15:14:26 -0400

    Create permute dYnAmIcALLy.py

---
+++ permute dYnAmIcALLy.py
@@ -0,0 +1,31 @@
+'''
+This code does the same as the recursive "permute.py", but, not recursively.
+This is a joke code, it's just a proof of concept that you don't *need* recursion to get a job done.
+'''
+
+
+size = int(input("size: "))
+filename = input("filename: ")
+
+output = f"file = open('{filename}.txt', 'w')\nfor i in range(ord('a'), ord('z') + 1):"
+iValues = ["chr(i)"]
+for i in range(size - 1):
+    iString = ''.join(['i' for x in range(i + 2)])
+    iValues.append(f"chr({iString})")
+    output += "\n"
+    for _ in range(i + 1):
+        output += '\t'
+    output += f"for {iString} in range(ord('a'), ord('z') + 1):"
+
+output += '\n'
+for _ in range(size):
+    output += '\t'
+output += f"file.write({' + '.join(iValues)} + ' ')"
+
+import time
+
+s = time.time()
+exec(output)
+print(time.time() - s)
+
+
