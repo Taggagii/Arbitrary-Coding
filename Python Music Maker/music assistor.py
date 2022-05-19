d4413de1ecab5e54043766752066e0758d10f434
Joshua Dietrich <Jdietrich0603@gmail.com>
2022-05-19 19:25:16 -0400

    fixing music thing

--- Python Music Maker/music assistor.py
+++ Python Music Maker/music assistor.py
@@ -2,6 +2,7 @@ from music_knowledge import *
 from midiutil.MidiFile import MIDIFile
 import os
 import pygame
+import random
 
 
 
@@ -29,7 +30,6 @@ def chord_maker(starting_note, chord_type):
     notes = [index + interval for interval in chord_values]
     return notes
 
-
 def build_chord_from_note(note, chord, numeric_value = False):
     if not numeric_value:
         note = note.capitalize()
@@ -47,9 +47,9 @@ def add_note(pitch, time = None, duration = 1):
         if time > lastTime:
             lastTime = time
     else:
-        lastTime += 1
+        lastTime += duration
         time =  lastTime
-    midi_file.addNote(track,  channel, pitch, time, duration, volume)
+    midi_file.addNote(track, channel, pitch, time, duration, volume)
 
 def add_chord(pitches, time = None, duration = 1):
     global lastTime
@@ -62,21 +62,61 @@ def add_chord(pitches, time = None, duration = 1):
     for pitch in pitches:
         add_note(pitch, time, duration)
 
+def add_random_scale(origin = 63):
+    scale = music_knowledge["scales"][random.choice(list(music_knowledge["scales"].keys()))]
+    for i in scale:
+        add_note(i + origin, duration = 1, increase_amount = random.randint(1, 2))
 
-origin = 65
+def add_random_sequence_from_scale(scale = None, length = None, origin = 63, random_origin = True, origin_change_frequency = 8):
+    if (scale is None):
+        scale = music_knowledge["scales"][random.choice(list(music_knowledge["scales"].keys()))]
+    else:
+        scale = music_knowledge["scales"][scale]
+    if (length is None):
+        length = random.randint(0, 50)
+    origin_change_counter = 0
+    value = random.randint(1, 80) / 10
+    for i in range(length):
+        if random_origin and origin_change_counter == origin_change_frequency:
+            origin = random.randint(20, 100)
+            origin_change_counter = 0
+            value = random.randint(1, 10) / 10
+        add_note(random.choice(scale) + origin, duration = value)
+        origin_change_counter += 1
 
-# for i in values:
-#     add_chord([origin + x for x in i], counter, 1)
-#     counter += 1
-# for i in music_knowledge['scales']['blues']:
-#     add_note(i + origin, counter, 1)
-#     counter += 1
+def add_random_chord():
+    chord_type = random.choice(list(music_knowledge["chords"].keys()))
+    starting_note = random.choice(music_knowledge["notes"])
+    add_chord(build_chord_from_note(starting_note, chord_type))
 
+def add_random_note():
+    starting_note = music_knowledge["notes"]
+    print(music_knowledge)
+
+##add_random_note()
+##for i in range(100):
+##    add_random_chord()
+
+for i in range(1000):
+    add_random_sequence_from_scale()
+##    for ii in range(random.randint(0, 1)):
+##        add_random_chord()
+
+
+origin = 50
+##for i in values:
+##    add_chord([origin + x for x in i], counter, 1)
+##    counter += 1
+
+
+
+
+##counter = 6
+##for i in [('Bb', 'major'), ('e', 'major'), ('f', 'minor'), ('g', 'diminished'), ('f#', 'minor'), ('c', 'major'), ('d', 'minor]:
+##    add_chord(build_chord_from_note(i[0], i[1]))
 counter = 6
-for i in [('Bb', 'major'), ('e', 'major')]:
-    add_chord(build_chord_from_note(i[0], i[1]))
     
-# add_note(music_knowledge['scales']['blues'][0] + origin + 12, counter, 5)
+##add_note(music_knowledge['scales']['blues'][0] + origin + 12, counter, 5)
 
 '''
 perfect unison = 0
